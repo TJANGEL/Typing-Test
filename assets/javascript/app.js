2e8c5743f3338cec9d9fc8f788cdbd90876dbad1
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-11-26 21:39:33 -0500

    update to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+window.addEventListener('load', init);
+
 // Global Variables
 let time = 5;
 let score = 0;
@@ -11,3 +13,64 @@ const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
 
+// possible words to type
+const words = [
+    // add words
+]
+
+// Initialize game
+function init() {
+    // Load word from array
+    showWord(words);
+
+    wordInput.addEventListener('input', startGame);
+
+    // Run countdown
+    setInterval(countdown, 1000);
+
+    // Check game status
+    setInterval(checkStatus, 50);
+}
+
+// Start Game
+function startGame() {
+    if (gameWords()) {
+        
+    }
+}
+
+// Match currentWord to wordInput
+function gameWords() {
+    if (wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct!!';
+        return true;
+    } else {
+        message.innerHTML = '';
+        return false;
+    }
+}
+
+
+// Randomly pick word from array
+function showWord(words) {
+    const randIndex = Math.floor(Math.random() * words.length);
+
+    currentWord.innerHTML = words[randIndex];
+}
+
+// Countdown
+function countdown() {
+    if (time > 0) {
+        time--;
+    } else if (time === 0) {
+        playing = false;
+    }
+    timeDisplay.innerHTML = time;
+}
+
+// Check game status
+function checkStatus() {
+    if (!playing && time === 0) {
+        message.innerHTML = 'Game Over!!';
+    }
+}
